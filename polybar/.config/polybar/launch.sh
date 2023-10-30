72a371ba3ce0d83a0c975cda7c15b9a76ca5aebc
Tuan Nguyen <tufourn@gmail.com>
2023-10-30 17:09:16 -0400

    polybar: multi monitor script

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,5 +1,10 @@
 # terminate already running bar instances
 polybar-msg cmd quit
 
-# launch bars
-polybar top
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
